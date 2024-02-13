223b0a963442fef80ae0ec859473a2c79f12df41
Kelilopz <kelilopz2171804@gmail.com>
2024-02-13 17:30:24 -0500

    listo

--- campers.py
+++ campers.py
@@ -1,7 +1,24 @@
 import json 
+#Escribir al Json                
+def guardarcambios(datos):
+    with open('campers.json','w') as archivo:
+        escritura = json.dumps(datos)
+        archivo.write(escritura)
+    
+
+#Leer al Json
+def CargarDatos(): 
+    try:   
+        with open('campers.json','r') as Campers:
+            data = json.load(Campers,indent=4)
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        data = []
+    return data
+
 #Funcion para ingresar campers
+
 def registroCampers():
-    ListaCampers=[]    
+    ListaCampers=list(CargarDatos())   
     while True:
         try:
             nombre=input("Escribe tu solo tus nombres\n")
@@ -12,25 +29,12 @@ def registroCampers():
             Telefono=int(input("Escribe tu numero de celular\n")) 
             Estado="En proceso de ingreso"
             Riesgo="Bajo"
+            Ruta=None
+            ListaCampers.append({'nombre': nombre, 'apellidos': apellidos, 'documento':documento,'direccion': direccion, 'Acudiente': Acudiente, 'Telefono': Telefono, 'Estado':Estado, 'Riesgo':Riesgo,'Ruta':Ruta})
+            guardarcambios(ListaCampers)
+            print("Usuario creado con exito")
             break
         except ValueError:
-            print("Por favor ingrese los valores indicados correctamente\n")
-    ListaCampers.append({'nombre': nombre, 'apellidos': apellidos, 'documento':documento,'direccion': direccion, 'Acudiente': Acudiente, 'Telefono': Telefono, 'Estado':Estado, 'Riesgo':Riesgo})
-    #Leer al Json
-    try:   
-        with open('campers.json','r') as Campers:
-            data = json.load(Campers)
-    except (FileNotFoundError, json.decoder.JSONDecodeError):
-        data = []
-                
-    #Añadir los datos a la lista existente
-    data.extend(ListaCampers)
-    
-    #Escribir al Json
-    with open('campers.json','w') as archivo:
-        json.dump(data,archivo, indent=4)
-    
-    
-    
-print("Hola mundo")
-registroCampers()        
+            print("Por favor ingrese los valores indicados correctamente\n")    
+
+registroCampers()
