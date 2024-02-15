6340b472b56f88ae9dce6f90c750c1323be2240c
Kelilopz <kelilopz2171804@gmail.com>
2024-02-14 22:20:06 -0500

    se hacen cambios en rutas y se crea rutas de aprendizaje como json

--- ruta.py
+++ ruta.py
@@ -5,41 +5,55 @@ import jsonsfunciones
 def CrearTema():
     listaTemas=jsonsfunciones.CargarDatos("rutas.json")
     try:
-        opcion=int(input("Que deseas cambiar?\n1.Fundamentos de programacion\n2.)Programacion web\n3.)Programacion formal\n4.)SGBD(Bases de datos)\n5.)Backend\n0.)Para salir\n"))
+        opcion=int(input("Que deseas cambiar?\n1.)Fundamentos de programacion\n2.)Programacion web\n3.)Programacion formal\n4.)SGBD(Bases de datos)\n5.)Backend\n0.)Para salir\n"))
         while True:
             if opcion==1:
                 Fundamentos_de_programacion=input("Escribe el nuevo fundamento\n")
-                listafundamentos=listaTemas['Fundamentos de programacion']
-                listafundamentos.append(Fundamentos_de_programacion)
-                jsonsfunciones.guardarcambios(listafundamentos)
+                listaTemas['fundamentosProgramacion'].append(Fundamentos_de_programacion)
+                jsonsfunciones.guardarcambios(listaTemas,"rutas.json")
                 print (listaTemas)
                 break
             elif opcion==2:
-                SGBD=input("Escribe tus SGBD\n")
-                listaTemas.append({'SGBD': SGBD})
-                guardarcambios(listaTemas)
-                print("Tema creado con exito")
+                ProgramacionWeb=input("Escribe el nuevo tema de programación Web\n")
+                listaTemas['Programacionweb'].append(ProgramacionWeb)
+                jsonsfunciones.guardarcambios(listaTemas,"rutas.json")
+                print (listaTemas)
                 break
+                
             elif opcion==3:
+                ProgramacionFormal=input("Escribe el nuevo tema de programación Formal\n")
+                listaTemas['ProgramacionFormal'].append(ProgramacionFormal)
+                jsonsfunciones.guardarcambios(listaTemas,"rutas.json")
+                print (listaTemas)
+                break
+            elif opcion==4:
+                SGBD=input("Escribe el nuevo tus SGBD\n")
+                listaTemas['SGBD'].append(SGBD)
+                jsonsfunciones.guardarcambios(listaTemas,"rutas.json")
+                print (listaTemas)
+                break
+            elif opcion==5:
                 Backend=input("Escribe tus Backend\n")
-                listaTemas.append({'Backend': Backend})
-                guardarcambios(listaTemas,"campers.json")
-                print("Tema creado con exito")
+                listaTemas['Backend'].append(Backend)
+                jsonsfunciones.guardarcambios(listaTemas,"rutas.json")
+                print (listaTemas)
+                break
+            elif opcion==0:
                 break
             else:
                 print("No elegiste una opción correcta, intentalo de nuevo")
     except Exception:
-        print("No escribiste un valor numerico intentalo de nuvo")
+        print("No escribiste un valor numerico intentalo de nuevo")
 
 #Crear una nueva ruta
-#def CrearRuta():
-#    lista_SGBD=list(CargarDatos())
-#    nombreruta=input("Por favor escribe el nomobre de la nueva ruta")
-#    
-#    while True:
-#       try:
-#           SGDB_principal=(int(input(
-#               "Por favor elige la Base de datos que deseas seleccionar principal"
-#                                     lista_SGBD)))
+def CrearRuta():
+    lista_rutas=jsonsfunciones.CargarDatos())
+    nombreruta=input("Por favor escribe el nomobre de la nueva ruta")
+    
+    while True:
+       try:
+           SGDB_principal=(int(input(
+               "Por favor elige la Base de datos que deseas seleccionar principal"
+                                     lista_SGBD)))
             
 CrearTema()
